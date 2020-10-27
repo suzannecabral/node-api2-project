204a9d98fccc2626e028878f84a98112ed30d873
Suzanne Cabral <suzannecabral@live.com>
2020-10-27 15:42:07 -0700

    Get post by id works with errors

--- posts/posts-router.js
+++ posts/posts-router.js
@@ -5,21 +5,21 @@ const Posts = require('../data/db');
 //endpoints-------------------------------------------
 //----------------------------------------------------
 
-// //test that this file is working=====================
+// //test that this file is working===================
 // router.get('/sillytest', (req,res)=>{
 //     res.json({message:'string'});
 // })
 
-// GET all posts =======================================
-//   
+// GET all posts =====================================
+
 router.get('/api/posts', (req,res) => {
     Posts.find(req.body)
         .then(data => {
             // throw ("error thing");
-            if(data){
-                res.status(200).json(data);
-            }else{
+            if(!data){
                 res.status(404).json({message:"Posts not found"});
+            }else{
+                res.status(200).json(data);
             }  
             
         })
@@ -29,6 +29,24 @@ router.get('/api/posts', (req,res) => {
         })
 });
 
+// GET post by id ====================================
+   
+router.get('/api/posts/:id', (req,res)=>{   
+    
+    Posts.findById(req.params.id)
+    .then(data => {
+            // throw("ERRRROOOOOORRR");
+            if(data.id !== req.params.id){
+                res.status(404).json({ message: "The post with the specified ID does not exist." })
+            }else{
+                res.status(200).json(data);
+            }
+        })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({ error: "The post information could not be retrieved." });
+    })
+});
 
 //----------------------------------------------------
 //endpoints-------------------------------------------
